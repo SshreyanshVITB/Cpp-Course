f515bc95f85ada0fa76b397054e15f0333695282
SshreyanshVITB <sshreyanshimp20@gmail.com>
2026-05-10 01:33:36 +0530

    Comments

---
+++ comments.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello CODERS";
+//     return 0;
+// }
+
+
+// this is used for single line comment
+/*
+This is
+used 
+for 
+multi
+line 
+comments
+*/
