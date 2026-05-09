55c4fa06a3e72b19bffb07c8d19df0fcf201da49
SshreyanshVITB <sshreyanshimp20@gmail.com>
2026-05-09 23:37:44 +0530

    Variable and Comments

---
+++ Variableandcomment.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+// This program was created by me
+/*This
+is
+a
+multi
+line
+comment
+*/
+int main(){
+    int sum = 6; //Variable assigned
+    cout<<"Hello world"<< sum;
+    return 0;
+}
+
+
