f09e9d777983362a7ddee9b1296a393990193c1d
SshreyanshVITB <sshreyanshimp20@gmail.com>
2026-05-11 01:00:06 +0530

    Variable and comment

---
+++ tut3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//This prog is created by shreyansh 
+/*This 
+is 
+a
+multi
+line
+comment
+*/
+int main(){
+    int sum = 6;
+    cout<<"hello world"<<sum;
+    
+    return 0;
+}
