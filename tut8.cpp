6202b7420ba04e00a9ef2f59ec9099837b597d65
SshreyanshVITB <sshreyanshimp20@gmail.com>
2026-05-12 00:13:57 +0530

    Tut8

--- tut8.cpp
+++ tut8.cpp
@@ -8,11 +8,11 @@ int main(){
 //     a = 34;
 //     cout<<"The value of a is :"<<a;
 
-//*********Constants in C++************
+// *********Constants in C++************
 // const int a = 45; //Const are used so that we cant change the value of the variable as shown above 
 //     cout<<"The value of a was :"<<a<<endl;
  
-    //     a = 34; // here u will get error
+//         a = 34; // here u will get error
 //     cout<<"The value of a is :"<<a;
 
 // ***************Manipulator is C++**************
@@ -26,7 +26,7 @@ int main(){
 // cout<<"The value of c is :"<<setw(4)<<c<<endl;
 
 
-//*********operator precedence************ You can see operator precedence from Cppreference.com
+// *********operator precedence************ You can see operator precedence from Cppreference.com
 int a=3 , b=4;
 //int c = (a*5)+b;
 int c = ((((a*5)+b)-45)+87);
